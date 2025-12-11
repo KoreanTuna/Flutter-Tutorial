c1a80c67de97bdfe97b10f507b55b20fcc33169e
KoreanTuna <songsee0528@gmail.com>
2025-12-11 14:14:37 +0900

    textfield callback

--- 04_assigment_1/basic_layout/lib/feature/text_input/text_input_screen.dart
+++ 04_assigment_1/basic_layout/lib/feature/text_input/text_input_screen.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
+/// 주희 대리님  : controller 선언 및 메모리 해제 / Stack으로 하지 않아도 될 것 같다
+/// immutable한 변수는 final로 선언하는 것이 좋다
+/// Entity를 통한 데이터 교환
+/// 변수 여러개를 만들어서 쓸 변수를 다르게 하는 것이 안전하다
+///
+///
+/// 수은 대리님 : Theme은 머테리얼 기본 위젯에도 적용되기 때문에, 사용하면 유용할 수 있다.
+/// stateful로 만든 이유?
+/// part of / part
+///
+///
+
 class TextInputScreen extends StatefulWidget {
   const TextInputScreen({super.key});
 
--- 04_assigment_1/basic_layout/lib/main.dart
+++ 04_assigment_1/basic_layout/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:basic_layout/feature/text_input/text_input_screen.dart';
+import 'package:basic_layout/feature/car/car_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: TextInputScreen(),
+      home: CarScreen(),
     );
   }
 }
